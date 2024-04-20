c3a77e4d2de1427727c1fc57802f5ed8b383e5ed
Keith Peters <kp@bit-101.com>
2024-04-20 13:24:17 -0400

    update blcairo

--- blcairo/main.go
+++ blcairo/main.go
@@ -4,41 +4,42 @@ package main
 import (
 	"github.com/bit101/bitlib/bllog"
 	"github.com/bit101/bitlib/blmath"
+	"github.com/bit101/bitlib/geom"
+	"github.com/bit101/bitlib/random"
 	cairo "github.com/bit101/blcairo"
 	"github.com/bit101/blcairo/render"
 	"github.com/bit101/blcairo/target"
 )
 
 func main() {
-	logfile := bllog.InitProjectLog("project.log")
-	defer logfile.Close()
+	bllog.InitProjectLog("project.log")
+	defer bllog.CloseLog()
 
 	renderTarget := target.Video
 
-	switch renderTarget {
-	case target.Image:
-		render.Image(800, 800, "out/out.png", scene1, 0.0)
-		render.ViewImage("out/out.png")
-		break
-
-	case target.Video:
+	if renderTarget == target.Image {
+		render.CreateAndViewImage(800, 800, "out/out.png", scene1, 0.0)
+	} else if renderTarget == target.Video {
 		program := render.NewProgram(400, 400, 30)
 		program.AddSceneWithFrames(scene1, 60)
-		program.RenderVideo("out/frames", "out/out.mp4")
-		render.PlayVideo("out/out.mp4")
-		break
+		program.RenderAndPlayVideo("out/frames", "out/out.mp4")
 	}
+}
+
+var (
+	points geom.PointList
+)
+
+func init() {
+	points = geom.RandomPointList(1000, 0, 0, 400, 400)
 }
 
 //revive:disable-next-line:unused-parameter
 func scene1(context *cairo.Context, width, height, percent float64) {
-	context.BlackOnWhite()
-	context.Save()
-	context.TranslateCenter()
-	context.DrawAxes(0.25)
-	r := blmath.LoopSin(percent, 50, width/2)
-	sphere := cairo.NewSphere(0, 0, r, 1, 0, 0)
-	sphere.SetShadowColor(0.2, 0, 0)
-	sphere.Draw(context)
-	context.Restore()
+	context.WhiteOnBlack()
+	random.Seed(0)
+	p := points.Clone()
+	r := blmath.LoopSin(percent, 0, 100)
+	p.Randomize(r, r)
+	context.Points(p, 1)
 }
