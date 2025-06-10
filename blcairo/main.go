0adb95d7a68bfce98d50f4aa77125c8bf410ee44
Keith Peters <kp@bit-101.com>
2025-06-10 19:52:08 -0400

    pov template. updates to blcairo main.go

--- blcairo/main.go
+++ blcairo/main.go
@@ -5,30 +5,53 @@ import (
 	"math"
 
 	"github.com/bit101/bitlib/blmath"
+	"github.com/bit101/bitlib/random"
 	cairo "github.com/bit101/blcairo"
 	"github.com/bit101/blcairo/render"
-	"github.com/bit101/blcairo/target"
 )
 
 //revive:disable:unused-parameter
 const (
+	// height = 270.0
+	height = 540.0
+	// height = 1080.0
+	// height = 2160.0
+
+	width       = height * render.Aspect16x9
+	baseHeight  = 1080.0
+	screenScale = height / baseHeight
+
 	tau = blmath.Tau
 	pi  = math.Pi
 )
 
 func main() {
-	renderTarget := target.Video
-	fileName := "${PROJECT_DIR}"
-
-	if renderTarget == target.Image {
-		render.CreateAndViewImage(600, 600, "out/"+fileName+".png", scene1, 0.0)
-	} else if renderTarget == target.Video {
-		program := render.NewProgram(600, 600, 30)
-		program.AddSceneWithFrames(scene1, 60)
-		program.RenderAndPlayVideo("out/frames", "out/"+fileName+".mp4")
+	singleImage := true
+	if singleImage {
+		doImage()
+	} else {
+		doMovie()
 	}
 }
 
-func scene1(context *cairo.Context, width, height, percent float64) {
+func doImage() {
+	path := "out/image.png"
+	render.Image(width, height, path, act1, 0)
+	render.ViewImage(path)
+}
+
+func doMovie() {
+	movie := render.NewMovie("movie", width, height, 30)
+	movie.NewAct("act1", 240, act1, true, false)
+	movie.CombineAll(true)
+}
+
+func act1(context *cairo.Context, width, height, percent float64) {
 	context.WhiteOnBlack()
+	context.SetLineWidth(0.5 * screenScale)
+	for range 100 {
+		context.MoveTo(random.FloatRange(0, width), random.FloatRange(0, height))
+		context.LineTo(random.FloatRange(0, width), random.FloatRange(0, height))
+	}
+	context.Stroke()
 }
---
+++ pov/main.pov
@@ -0,0 +1,42 @@
+#include "stdinc.inc"
+#include "textures.inc"
+#include "stones.inc"
+#include "skies.inc"
+
+light_source {
+  <-4, 4, -3>
+    White
+}
+
+light_source {
+  <8, 8, 0>
+    White 
+}
+
+camera {
+  right x * image_width / image_height
+    location <2, 2, -4>
+    look_at  <0, 0, 0>
+}
+
+box {
+  <-1, -1, -1>
+  <1, 1, 1>
+
+  texture {
+    T_Stone10
+  }
+  rotate clock*360
+}
+
+sky_sphere {
+  pigment {
+    gradient y
+    color_map {
+      [0 color Red]
+      [1 color Blue]
+    }
+    scale 2
+    translate -1
+  }
+}
